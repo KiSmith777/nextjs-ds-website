7b065457421f75e233231035cc6021b38b2ae54a
DSFounder <chris@defiskeptic.com>
2022-08-08 17:05:11 -0400

    contract address padding

--- src/containers/token-nft/index.jsx
+++ src/containers/token-nft/index.jsx
@@ -77,11 +77,12 @@ const TokenNFTArea = ({ className, space, data }) => {
                             </div>
                             <div className="row text-center">
                                 <p
+                                    style={{paddingLeft: "4px", paddingRight: "4px"}}
                                     data-sal="slide-right"
                                     data-sal-duration="1200"
                                     data-sal-delay="150"
                                 >
-                                    BSC Address
+                                    Token Address
                                     <br />
                                     0x1234AE511876FCAaCe685fcDC292d9589A88dC2b
                                 </p>
@@ -152,11 +153,12 @@ const TokenNFTArea = ({ className, space, data }) => {
                             </div>
                             <div className="row text-center">
                                 <p
+                                    style={{paddingLeft: "4px", paddingRight: "4px"}}
                                     data-sal="slide-right"
                                     data-sal-duration="1200"
                                     data-sal-delay="150"
                                 >
-                                    BSC Address
+                                    NFT Address
                                     <br />
                                     0xe82d1E44a1f8a37f74A718Ee797F29Eb3aE1D84A
                                 </p>
