aab54e3b7ac198d4d153728b334eada92de1aad1
GiverTheWall <ksmitty727@gmail.com>
2022-08-23 22:33:22 -0400

    centered & sized embed

--- src/containers/fiat-onboarding/index.jsx
+++ src/containers/fiat-onboarding/index.jsx
@@ -29,16 +29,22 @@ const FIATArea = ({ space, className }) => {
                     >
                         <div className="section-title mb--30">
                             <h2 className="title text-center">
-                                FIAT Onboarding 
+                                FIAT Onboarding
                             </h2>
                             <TransparentBox>
                                 <p className="description text-justify">
-                                <h4>FIAT to Token has never been easier!</h4>
-                                Have you ever wanted to easily implement a FIAT buying method for your project
-                                so your userbase could seamlessly buy your token? With this plug and play option,
-                                its as simple as adding some ebeded source code to your site. With FIAT Onboarding, 
-                                you can instantly give your users the ability to buy your Token using various payment 
-                                methods such as:
+                                    <h4>
+                                        FIAT to Token has never been easier!
+                                    </h4>
+                                    Have you ever wanted to easily implement a
+                                    FIAT buying method for your project so your
+                                    userbase could seamlessly buy your token?
+                                    With this plug and play option, its as
+                                    simple as adding some ebeded source code to
+                                    your site. With FIAT Onboarding, you can
+                                    instantly give your users the ability to buy
+                                    your Token using various payment methods
+                                    such as:
                                 </p>
                                 <ul>
                                     <li>Apple Pay</li>
@@ -47,10 +53,13 @@ const FIATArea = ({ space, className }) => {
                                     <li>Bank Transfer</li>
                                 </ul>
                                 <p>
-                                The best part is...its FREE! You can utilize FIAT Onboarding for free, however if you 
-                                would like to have your project "verified" this would require a KYC that incurs a cost. 
-                                For more information or to get started, click the button below. You can also see an 
-                                example of this overlay below as well.
+                                    The best part is...its FREE! You can utilize
+                                    FIAT Onboarding for free, however if you
+                                    would like to have your project "verified"
+                                    this would require a KYC that incurs a cost.
+                                    For more information or to get started,
+                                    click the button below. You can also see an
+                                    example of this overlay below as well.
                                 </p>
                                 <div
                                     className={clsx(
@@ -79,26 +88,28 @@ const FIATArea = ({ space, className }) => {
                                         show && "showForm"
                                     )}
                                 />
-                                < br/>< br/>< br/>
-                                <h5 className="title text-center">Example of FIAT Onboarding Embed</h5>
-                                < br/>
-                                <p className="title text-center">Preview Coming Soon</p>
-                                {/*<div
-                                    style={{ 
-                                        display: "flex",
-                                        alignitems: "center",
-                                        justifycontent: "center",
-                                        maxWidth: "35%"}}
-                                    >
-                                    <iframe src="https://flooz.trade/embedded/0x1234AE511876FCAaCe685fcDC292d9589A88dC2b/?refId=QHd5vF&backgroundColor=black&align=center" width="400" height="660" allow="accelerometer; autoplay; camera; gyroscope; payment" />
-                                    </div>*/}
+                                <br />
+                                <br />
+                                <br />
+                                <h5 className="title text-center">
+                                    Example of FIAT Onboarding Embed
+                                </h5>
+                                <br />
+                            
+                                <div className="d-flex justify-content-center">
+                                    <iframe
+                                        src="https://flooz.trade/embedded/0x1234AE511876FCAaCe685fcDC292d9589A88dC2b/?refId=QHd5vF&backgroundColor=black&align=center"
+                                        width="400"
+                                        height="660"
+                                        allow="accelerometer; autoplay; camera; gyroscope; payment"
+                                    />
+                                </div>
                             </TransparentBox>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-        
     );
 };
 
@@ -112,3 +123,4 @@ FIATArea.defaultProps = {
 };
 
 export default FIATArea;
+
