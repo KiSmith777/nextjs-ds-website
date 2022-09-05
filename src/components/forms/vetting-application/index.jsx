e169a6b1466dd783d5d41cb8a47004b445fb7d89
DSFounder <chris@defiskeptic.com>
2022-09-05 08:58:58 -0400

    changed regex in vetting application

--- src/components/forms/vetting-application/index.jsx
+++ src/components/forms/vetting-application/index.jsx
@@ -20,7 +20,8 @@ const VettingApplication = ({ className }) => {
     function handleWebsiteInput(e) {
         let value = e.target.value;
         let regexTest =
-            /^[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_\+.~#?&//=]*)$/;
+            /^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,7}(:[0-9]{1,5})?(\/.*)?$/
+            {/*/^[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_\+.~#?&//=]*)$/;*/}
 
         if (regexTest.test(value) === false) {
             console.log(value);
